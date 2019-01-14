2e41993bc49cfb3e756726752abe369741566ab1
sadscv <sadscv@hotmail.com>
2019-01-14 11:45:32 +0800

    * refactor examArrangement.py create Class examArranger() * add delete_table fun in utils.py

--- AutoExam/examArrangement.py
+++ AutoExam/examArrangement.py
@@ -1,44 +1,42 @@
 from datetime import datetime
 
-import pypyodbc as pypyodbc
+import pypyodbc
 import pandas as pd
 
-from AutoExam.utils import testTable
+from AutoExam.utils import testTable, delete_table
 
-def connectDB():
-    """
-    connect Access database
-    :return:connection
-    """
-    db_file = r'''C:\Users\sadscv\Desktop\考试安排测试.accdb'''
-    #Todo update database config.
-    user = ''
-    password = ''
-    odbc_conn_str = "DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;UID=%s;PWD=%s"%(db_file, user, password)
-    conn = pypyodbc.connect(odbc_conn_str)
-    return conn
 
+class ExamArranger():
+    def __init__(self, db_file=None):
+        self.db_file = db_file
+        self.conn = self.connectDB(db_path=db_file)
+        self.cursor = self.conn.cursor()
+        self.check_para()
+
+    def connectDB(self, db_path):
+        """
+        connect Access database
+        :return:connection
+        """
+        #Todo update database config.
+        user = ''
+        password = ''
+        odbc_conn_str = "DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;UID=%s;PWD=%s"%(self.db_file, user, password)
+        conn = pypyodbc.connect(odbc_conn_str)
+        return conn
+
+    def check_para(self):
+        if not testTable("Para", self.cursor):
+            str = "create table Para(ksxq datetime,A integer, B byte)"
+            self.cursor.execute(str)
+            str = 'insert into Para values (#3/1/2004#,0,0)'
+            self.cursor.execute(str)
+            self.conn.commit()
 
 def empty_clsroom(conn):
     query = "SELECT * FROM 历届十佳 where ID=2;"
     data = pd.read_sql(query,conn)
     print(data)
-
-def init():
-    """
-    init database connection.
-    check if Para table exist.
-    :return: conn
-    """
-    conn = connectDB()
-    cursor = conn.cursor()
-    if not testTable("Para", cursor):
-        str = "create table Para(ksxq datetime,A integer, B byte)"
-        cursor.execute(str)
-    str = 'insert into Para values (#3/1/2004#,0,0)'
-    cursor.execute(str)
-    conn.commit()
-    return conn
 
 
 def update_para(cursor=None):
@@ -55,8 +53,11 @@ def update_para(cursor=None):
         return True
     except:
         pass
+
+
 if __name__ == '__main__':
-    conn = init()
-    update_para(conn.cursor())
+    db_file = r'''C:\Users\sadscv\Desktop\考试安排测试.accdb'''
+    arranger = ExamArranger(db_file=db_file)
+    update_para(arranger.cursor())
     # testTable('历届百佳',conn.cursor())
 
--- AutoExam/utils.py
+++ AutoExam/utils.py
@@ -5,5 +5,10 @@ def testTable(tablename,cursor):
         return False
 
 
+def delete_table(tablename, cursor):
+    if testTable(tablename, cursor):
+        cursor.execute("drop table {}".format(str(tablename)))
+        print('deleting table:{}'.format(tablename))
+        cursor.commit()
 
 
